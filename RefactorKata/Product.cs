be1df6beb1d6d1ed77119a99c52523c1c930b54c
Wes Jarvis <wes.b.jarvis@gmail.com>
2018-01-23 21:43:31 -0600

    Added a product class log

---
+++ RefactorKata/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RefactorKata
+{
+    public class Product
+    {
+        public string name;
+        public string Name { get { return name; } set { name = value; } }
+    }
+}
+
